643d7e36f164194c4461ffff4f3293ff2f91d072
Mirkomol <mirkamolrakhimov@gmail.com>
2025-04-28 15:00:09 +0300

    Update settings.py

--- maango/settings.py
+++ maango/settings.py
@@ -14,7 +14,12 @@ SECRET_KEY = 'django-insecure-kxwc3-(&i04+&-ym4ddm@um2_2bjqaiwg&7b4(k@&h#1)#k7q1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['test-maang-9.onrender.com']
+ALLOWED_HOSTS = ['test-maang-9.onrender.com','*']
+
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://test-maang-9.onrender.com',
+]
 
 
 # Application definition
